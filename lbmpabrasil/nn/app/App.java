367dad461729e700a8c011e8349c387bb365b64a
lbm-pa-brasil <107805598+lbm-pa-brasil@users.noreply.github.com>
2022-06-20 20:03:14 -0300

    Add files via upload

--- lbmpabrasil/nn/app/App.java
+++ lbmpabrasil/nn/app/App.java
@@ -1,5 +1,7 @@
 package lbmpabrasil.nn.app;
 
+import java.util.Random;
+
 import javax.swing.SwingUtilities;
 
 import lbmpabrasil.nn.neuron.*;
@@ -17,16 +19,38 @@ public class App {
 		Neuron neuron = new FixedBIASNeuron();
 		neuron.setTransferFunction(tf);
 		
-		double[] weights = new double[] {1.0d, 2.0d};
-		neuron.setWeights(weights);
-		
-		double[] inputs = new double[] {1.0d, 2.0d};
-		neuron.setInputs(inputs);	
-		
 		((FixedBIASNeuron) neuron).setBIASWeight(1.0d);
 		
-		System.out.println("Se tudo estiver certo, o valor 0.9975273768433653 deve aparecer no resultado!");
-		System.out.println(neuron.getYk());
+		{
+			double[] weights = new double[] {1.0d, 2.0d};
+			neuron.setWeights(weights);
+			
+			double[] inputs = new double[] {1.0d, 2.0d};
+			neuron.setInputs(inputs);
+			
+			System.out.println("Se tudo estiver certo, o valor 0.9975273768433653 deve aparecer no resultado!");
+			System.out.println(neuron.getYk());
+		}
+		
+		{	
+			System.out.println("Testando a performance para 1000000000 de entradas...");	
+			
+			for(int step = 1; step < 100000000; step *= 10) {
+				double[] weights = new double[step];
+				for(int it = 0; it < step; it++) 
+					weights[it] = (new Random()).nextDouble();
+				neuron.setWeights(weights);
+				
+				double[] inputs = new double[step];
+				for(int it = 0; it < step; it++) 
+					inputs[it] = (new Random()).nextDouble();
+				neuron.setInputs(inputs);	
+				
+				long time = System.currentTimeMillis();
+				double output = neuron.getYk();
+				System.out.println(step + " iterações concluídas em " + (System.currentTimeMillis() - time) + " milissegundos.");
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
