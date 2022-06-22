fb360597e3393c1cea2bc49686d0375ec07e663c
lbm-pa-brasil <107805598+lbm-pa-brasil@users.noreply.github.com>
2022-06-22 14:06:47 -0300

    Add files via upload

--- src/lbmpabrasil/nn/app/App.java
+++ src/lbmpabrasil/nn/app/App.java
@@ -17,15 +17,15 @@ import lbmpabrasil.nn.neuron.transfer.TransferFunction;
  */
 public class App {	
 	public void wakeUp() {		
-		TransferFunction tf = new SigmoidFunction();
-		tf.setAlpha(1.0d);
-		
-		Neuron neuron = new FixedBIASNeuron();
-		neuron.setTransferFunction(tf);
-		
-		((FixedBIASNeuron) neuron).setBIASWeight(1.0d);
-		
 		{
+			TransferFunction tf = new SigmoidFunction();
+			tf.setAlpha(1.0d);
+			
+			Neuron neuron = new FixedBIASNeuron();
+			neuron.setTransferFunction(tf);		
+			
+			((FixedBIASNeuron) neuron).setBIASWeight(1.0d);
+			
 			double[] weights = new double[] {1.0d, 2.0d};
 			neuron.setWeights(weights);
 			
@@ -37,6 +37,14 @@ public class App {
 		}
 		
 		{	
+			TransferFunction tf = new SigmoidFunction();
+			tf.setAlpha(1.0d);
+			
+			Neuron neuron = new FixedBIASNeuron();
+			neuron.setTransferFunction(tf);		
+			
+			((FixedBIASNeuron) neuron).setBIASWeight(1.0d);
+			
 			System.out.println("Testando a performance...");	
 			
 			for(int step = 1; step < 100000000; step *= 10) {
@@ -56,26 +64,28 @@ public class App {
 			}
 		}
 		
-		
-		
-		
-		
-		
-		
-		
 		{
+			TransferFunction tf = new SigmoidFunction();
+			tf.setAlpha(1.0d);
+			
 			NeuralNetwork nn = new ForwardNetworkSingleThread();
-			nn.createNetwork(tf, new double[] {3, 2, 2, 2});
+			nn.createNetwork(tf, new double[] {32 * 32, 2, 2, 2});
+			
+			
+			double[] outputs = nn.getOutputs();
+			
+			
+			for(int it = 0; it < outputs.length; it++)
+				System.out.println(outputs[it]);
+			
+			
+			
+			
+			
+			
 			
 			
 		}
-		
-		
-		
-		
-		
-		
-		
 	}
 	
 	public static void main(String[] args) {
--- src/lbmpabrasil/nn/network/AbstractNeuronNetwork.java
+++ src/lbmpabrasil/nn/network/AbstractNeuronNetwork.java
@@ -30,31 +30,34 @@ public abstract class AbstractNeuronNetwork implements NeuralNetwork {
 			this.inputs[it] = 0.0d;
 		
 		/*
+		 *  CRIA A CAMADA DE SAÍDA DA REDE.
+		 */
+		this.outputs = new double[(int) config[config.length - 1]]; 
+		for(int it = 0; it < (int) config[config.length - 1]; it++) 
+			this.outputs[it] = 0.0d;		
+		
+		/*
 		 *  CRIA AS CAMADAS RESTANTES.
 		 */
 		for(int it = 1; it < config.length; it++) {
-			this.neurons[it] = new FixedBIASNeuron(); // FUTURAMENTE, DELEGAR PARA UMA FACTORY A CRIAÇÃO, PASSANDO O TIPO DE NEURÔNIO.
-			this.neurons[it].setTransferFunction(tf);			
-			((FixedBIASNeuron) this.neurons[it]).setBIASWeight(1.0d);
-			
-			double[] inputs = new double[(int) config[it - 1]];
-			double[] weights = new double[(int) config[it - 1]];
-			
-			for(int it1 = 0; it1 < (int) config[it - 1]; it1++) {
-				inputs[it] = 0.0d;
-				weights[it] = (new Random()).nextDouble();
+			for(int it1 = 0; it1 < (int) config[it]; it1++) {
+				this.neurons[it] = new FixedBIASNeuron(); // FUTURAMENTE, DELEGAR PARA UMA FACTORY A CRIAÇÃO, PASSANDO O TIPO DE NEURÔNIO.
+				this.neurons[it].setTransferFunction(tf);	
+				
+				double[] inputs = new double[(int) config[it - 1]];
+				double[] weights = new double[(int) config[it - 1]];				
+				
+				for(int it2 = 0; it2 < (int) config[it - 1]; it2++) {
+					inputs[it2] = 0.0d;
+					weights[it2] = (new Random()).nextDouble();
+				}
+				
+				this.neurons[it].setInputs(inputs);	
+				this.neurons[it].setWeights(weights);
+				
+				((FixedBIASNeuron) this.neurons[it]).setBIASWeight(1.0d);
 			}
-			
-			this.neurons[it].setInputs(inputs);	
-			this.neurons[it].setWeights(weights);
 		}
-		
-		
-		... PAREI AQUI ...
-		
-		
-		
-		
 	}	
 	
 	protected abstract void process(); // PLACEHOLDER METHOD. NEGÓCIO DELEGADO PARA CLASSE FILHA.
